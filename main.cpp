42cf532c644ac108e2cebe645ec675e7ad4348e6
addrave <addrave@gmail.com>
2019-02-02 17:03:17 +0300

    no message

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <thread>
 #include <QDebug>
+#include <windows.h>
 
 using namespace std;
 
@@ -29,6 +30,7 @@ class MyMap
     }
     void show()
     {
+        this->depth = 0;
         count_deth(Tree, 0);
         cout << '\n' << "depth " << depth << '\n';
         show(Tree);
@@ -63,15 +65,79 @@ class MyMap
         }
     }
 
-    void del(Node<T, T2> *&Tree) {
-        if (Tree != NULL) //Пока не встретится пустое звено
+    void del(T key)
+    {
+        Node<T, T2> **temp = NULL;
+        temp = get_value_(key, Tree, Tree);
+        if (temp == NULL)
         {
-            del(Tree->l); //Рекурсивная функция прохода по левому поддереву
-            del(Tree->r); //Рекурсивная функци для прохода по правому поддереву
-            delete Tree; //Убиваем конечный элемент дерева
-            Tree = NULL; //Может и не обязательно, но плохого не будет
+            qDebug() << "ITEM NOT FOUND (DEl) whith key " << key << endl;
+            return void();
         }
+        if (temp[0] == Tree)
+        {
+            qDebug() << "ITEM HEAD OF THIS TREE" << endl;
+            return void();
+        }
+        temp[0]->mtx.lock();
+        temp[1]->mtx.lock();
+        qDebug() << '\n' << temp[0]->x << '\t' << temp[1]->x << '\t';
+        if (temp[0]->l == NULL && temp[0]->r == NULL)
+        {
+            qDebug() << "object without children" << endl;
+            if (temp[1]->l == temp[0])
+                temp[1]->l = NULL;
+            if (temp[1]->r == temp[0])
+                temp[1]->r = NULL;
+            temp[0]->mtx.unlock();
+            temp[1]->mtx.unlock();
+            delete temp[0];
+            temp[0] = NULL;
+            return void();
+        }
+        if (temp[0]->l == NULL && temp[0]->r != NULL)
+        {
+            qDebug() << "object with only one RIGHT children" << endl;
+            if (temp[1]->l == temp[0])
+                temp[1]->l = temp[0]->r;
+            if (temp[1]->r == temp[0])
+                temp[1]->r = temp[0]->r;
+            temp[0]->mtx.unlock();
+            temp[1]->mtx.unlock();
+            delete temp[0];
+            temp[0] = NULL;
+            return void();
+        }
+        if (temp[0]->l != NULL && temp[0]->r == NULL)
+            qDebug() << "object with only one LEFT children" << endl;
+        if (temp[1]->l == temp[0])
+            temp[1]->l = temp[0]->l;
+        if (temp[1]->r == temp[0])
+            temp[1]->r = temp[0]->l;
+        temp[0]->mtx.unlock();
+        temp[1]->mtx.unlock();
+        delete temp[0];
+        temp[0] = NULL;
+        return void();
+        if (temp[0]->l != NULL && temp[0]->r != NULL && temp[0] != temp[1])
+        {
+            qDebug() << "OBJECT WHITH 2 CHILD AND PARENT";
+//            T deletedX = temp[0]->x;
+//            Node<T, T2> node;
+//            while (node.)
+//            {
 
+//            }
+        }
+//        if (Tree != NULL) //Пока не встретится пустое звено
+//        {
+//            del(Tree->l); //Рекурсивная функция прохода по левому поддереву
+//            del(Tree->r); //Рекурсивная функци для прохода по правому поддереву
+//            delete Tree; //Убиваем конечный элемент дерева
+//            Tree = NULL; //Может и не обязательно, но плохого не будет
+//        }
+        temp[0]->mtx.unlock();
+        temp[1]->mtx.unlock();
     }
 
     void add_node(T x, T2 x2)
@@ -134,21 +200,30 @@ class MyMap
 
     T2 get_value(T key)
     {
-        pair<T2, bool> temp;
-        temp = get_value_(key, Tree);
-        if (temp.second == true)
-            qDebug() << "\n Find! key = " << key << " val = " << temp.first << endl;
+        Node<T, T2> **temp = NULL;
+        temp = get_value_(key, Tree, Tree);
+//        cout << temp[0]->x << "AAAAAAAAAAA";
+        if (temp != NULL)
+        {
+            //MUTEX!
+            qDebug() << "\n Find! key = " << temp[0]->x << " val = " << temp[0]->x2 << endl;//MUTEX!
+        }
         else
             cout << "\n Not find item with key = " << key << endl;
-        return temp.first;
+        //MUTEX!
+        return temp[0]->x;//MUTEX!
     }
 
-    pair<T2, bool> get_value_(T key, Node<T, T2> *&MyTree)
+    Node<T, T2>** get_value_(T key, Node<T, T2> *&MyTree, Node<T, T2> *&ParentTree)
     {
+//        cout << MyTree->x << "aa";
         MyTree->mtx.lock();
         if (MyTree->x == key)
         {
-            pair<T2, bool> temp; temp.first = MyTree->x2; temp.second = true;
+//            cout << "AAAAAAAAAAAAA";
+            Node<T, T2> **temp = new Node<T, T2>* [2] {NULL, NULL}; // первый эдемент с нужным ключем второй элемент родитель данного
+            temp[0] = MyTree;
+            temp[1] = ParentTree;
             MyTree->mtx.unlock();
             return temp;
         }
@@ -157,17 +232,16 @@ class MyMap
             Node<T, T2> *tempL = MyTree->l;
             Node<T, T2> *tempR = MyTree->r;
             MyTree->mtx.unlock();
-            if (tempL != NULL && get_value_(key, tempL).second == true)
+            if (tempL != NULL && get_value_(key, tempL, MyTree) != NULL)
             {
-                return get_value_(key, tempL);
+                return get_value_(key, tempL, MyTree);
             }
-            if (tempR != NULL && get_value_(key, tempR).second == true)
+            if (tempR != NULL && get_value_(key, tempR, MyTree) != NULL)
             {
-                return get_value_(key, tempR);
+                return get_value_(key, tempR, MyTree);
             }
         }
-        pair<T2, bool> temp; temp.first = NULL; temp.second = false;
-        return temp;
+        return NULL;
     }
 
 };
@@ -180,26 +254,27 @@ int main()
     obj.add_node(34,1);
     obj.add_node(51,111);
     obj.add_node(52,1);
+    obj.add_node(54,1);
     obj.add_node(53,1);
 //    obj.add_node(50,0);
     obj.add_node(50,0);
     obj.add_node(94,1);
     obj.add_node(93,1);
     obj.add_node(100,1);
-//    obj.show();
+    obj.show();
 //    qDebug() << obj.get_value(52);
 //    thread t1 (&MyMap<int,int>::add_node, obj, 1, 1);
 //    thread t2 (&MyMap<int,int>::add_node, obj, 1, 1);
 //    t1.join();
 //    t2.join();
-    thread t1 (&MyMap<int,int>::get_value, obj, 50);
-    thread t2 (&MyMap<int,int>::get_value, obj, 51);
-    thread t3 (&MyMap<int,int>::get_value, obj, 510);
+    thread t1 (&MyMap<int,int>::del, obj, 52);
+    thread t2 (&MyMap<int,int>::del, obj, 52);
+    thread t3 (&MyMap<int,int>::del, obj, 52);
     t1.join();
     t2.join();
     t3.join();
 
-//    obj.show();
+    obj.show();
 
 //    cout << ("11kkj0" < "1kkj0") ? true : false;
 //    cout << strcmp("11kkj0", "1kkj0") ? true:false;
